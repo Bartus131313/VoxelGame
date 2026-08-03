a516aafbe45a1aead74527031de1bbf1049fde31
Bartus131313 <bartek.kansy@pisz.to>
2026-08-03 17:15:10 +0200

    Update Camera3D

--- src/game/render/camera/Camera3D.cpp
+++ src/game/render/camera/Camera3D.cpp
@@ -3,8 +3,8 @@
 #include "glm/ext/matrix_clip_space.hpp"
 #include "glm/ext/matrix_transform.hpp"
 
-Camera3D::Camera3D(glm::vec3 position, const float yaw, const float pitch, const float fov, const float aspectRatio)
-        : m_position(position), m_yaw(yaw), m_pitch(pitch), m_fov(fov), m_aspectRatio(aspectRatio) {
+Camera3D::Camera3D(const glm::vec3 position, const float yaw, const float pitch, const float fov, const float aspectRatio)
+        : yaw(yaw), pitch(pitch), m_position(position), m_fov(fov), m_aspectRatio(aspectRatio) {
     updateVectors();
 }
 
@@ -12,29 +12,17 @@ void Camera3D::updateAspect(const int width, const int height) {
     if (height > 0) m_aspectRatio = static_cast<float>(width) / static_cast<float>(height);
 }
 
-// void Camera3D::processMouseMovement(float xOffset, float yOffset, bool constrainPitch) {
-//     m_yaw += xOffset;
-//     m_pitch += yOffset;
-//
-//     // Clamp pitch to prevent camera flip (gimbal lock)
-//     if (constrainPitch) {
-//         m_pitch = std::clamp(m_pitch, -89.0f, 89.0f);
-//     }
-//
-//     updateVectors();
-// }
-
 void Camera3D::updateVectors() {
     // Calculate new Front vector from Euler angles
     glm::vec3 front;
-    front.x = static_cast<float>(cos(glm::radians(m_yaw)) * cos(glm::radians(m_pitch)));
-    front.y = static_cast<float>(sin(glm::radians(m_pitch)));
-    front.z = static_cast<float>(sin(glm::radians(m_yaw)) * cos(glm::radians(m_pitch)));
+    front.x = static_cast<float>(cos(glm::radians(yaw)) * cos(glm::radians(pitch)));
+    front.y = static_cast<float>(sin(glm::radians(pitch)));
+    front.z = static_cast<float>(sin(glm::radians(yaw)) * cos(glm::radians(pitch)));
 
     m_front = glm::normalize(front);
 
     // Recalculate Right and Up vectors
-    m_right = glm::normalize(glm::cross(m_front, m_worldUp));
+    m_right = glm::normalize(glm::cross(m_front, WORLD_UP));
     m_up    = glm::normalize(glm::cross(m_right, m_front));
 }
 
--- src/game/render/camera/Camera3D.h
+++ src/game/render/camera/Camera3D.h
@@ -11,6 +11,7 @@ class Camera3D {
 public:
     static constexpr float NEAR_PLANE = 0.01f;
     static constexpr float FAR_PLANE  = 1000.0f;
+    static constexpr auto  WORLD_UP   = glm::vec3{0.0f, 1.0f, 0.0f};
 
     /// Constructs a new Camera3D instance with position and projection parameters.
     /// @param position Initial position vector in 3D world space (default: [0, 0, 0]).
@@ -28,7 +29,7 @@ public:
     void updateAspect(int width, int height);
 
     /// Recalculates the internal <b>Front</b>, <b>Right</b>, and <b>Up</b> directional vectors.
-    /// Must be called whenever @c m_yaw or @c m_pitch values are modified.
+    /// Must be called whenever @c yaw or @c pitch values are modified.
     void updateVectors();
 
     /// Computes and returns the 3D View matrix.
@@ -65,15 +66,14 @@ public:
     /// @return Camera's field of view (FOV).
     [[nodiscard]] float getFov() const { return m_fov; }
 
+    float yaw{-90.0f};    ///< Rotation of the camera around vertical axis (left, right).
+    float pitch{0.0f};    ///< Rotation of the camera around lateral axis (up, down).
+
 private:
     glm::vec3 m_position{0.0f};             ///< Position of the camera in 3D world.
     glm::vec3 m_front{0.0f, 0.0f, -1.0f};   ///< Front vector of the camera.
     glm::vec3 m_up{0.0f, 1.0f, 0.0f};       ///< Up vector of the camera.
     glm::vec3 m_right{1.0f, 0.0f, 0.0f};    ///< Right vector of the camera.
-    glm::vec3 m_worldUp{0.0f, 1.0f, 0.0f};  ///< Global up vector of the world.
-
-    float m_yaw{-90.0f};    ///< Rotation of the camera around vertical axis (left, right).
-    float m_pitch{0.0f};    ///< Rotation of the camera around lateral axis (up, down).
 
     float m_fov{60.0f};                 ///< Field of view of the camera.
     float m_aspectRatio{16.0f / 9.0f};  ///< Aspect ratio of the camera's viewport.
