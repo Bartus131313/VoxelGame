981a0371bb59c3df165a715e784fa64fba208fe7
Bartus131313 <bartek.kansy@pisz.to>
2026-08-04 15:05:49 +0200

    Update Mesh.h

--- src/game/render/mesh/Mesh.h
+++ src/game/render/mesh/Mesh.h
@@ -21,11 +21,6 @@ public:
     Mesh& operator=(Mesh&&) noexcept = default;
 
     /**
-     * @brief Binds the vertex array and issues the appropriate OpenGL draw call.
-     */
-    virtual void render() const = 0;
-
-    /**
      * @brief Binds the underlying Vertex Array Object (VAO).
      */
     virtual void bind() const = 0;
@@ -36,7 +31,13 @@ public:
     virtual void unbind() const = 0;
 
     /**
+     * @brief Binds the vertex array and issues the appropriate OpenGL draw call.
+     */
+    virtual void render() const = 0;
+
+    /**
      * @brief Gets the total number of indices or vertices to render.
+     *
      * @return Element draw count.
      */
     [[nodiscard]] virtual std::size_t getCount() const = 0;
